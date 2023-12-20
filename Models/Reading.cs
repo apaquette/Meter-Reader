5d8ccf3931521f6cdccf048ee33edc56077c6449
Alex <alexandre.d.paquette@gmail.com>
2023-12-20 14:53:26 +0000

    Modify Reading class to match design model

---
+++ Models/Reading.cs
@@ -0,0 +1,7 @@
+﻿namespace MeterReader.Models;
+public class Reading
+{
+    public Meter EnergyMeter { get; private set; }
+    public DateTime Time { get; private set; }
+    public decimal Amount { get; private set; }
+}
