711287f2dee9ee4cd8f7d5592b92abd7c5eb77cb
Alex <alexandre.d.paquette@gmail.com>
2023-12-20 14:57:15 +0000

    Added Meter class

---
+++ Models/Meter.cs
@@ -0,0 +1,27 @@
+﻿namespace MeterReader.Models;
+
+public class Meter
+{
+    private Microcontroller microcontroller;
+
+    public string SerialNumber { get; }
+
+    public Meter(Microcontroller controller, string serialNumber)
+    {
+        microcontroller = controller;
+        SerialNumber = serialNumber;
+    }
+
+
+
+    //TODO: Implement GetReadings
+    public List<Reading> GetReadings()
+    {
+        return new();
+    }
+
+    public void AssignMicrocontroller(Microcontroller controller)
+    {
+        microcontroller = controller;
+    }
+}
