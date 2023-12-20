de5d0ce9c26f377a6b8caf286d630b116b7bb9f0
Alex <alexandre.d.paquette@gmail.com>
2023-12-20 15:04:21 +0000

    Added constructor for microcontroller

--- Models/Microcontroller.cs
+++ Models/Microcontroller.cs
@@ -3,6 +3,11 @@
 public class Microcontroller
 {
     private Meter? currentMeter;
+    public Microcontroller(Meter? currentMeter, int id)
+    {
+        this.currentMeter = currentMeter;
+        Id = id;
+    }
 
     public int Id { get; private set; }
 }
