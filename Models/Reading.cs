52a188eeff1861f71c2f623ee29c6440921026cc
Alex <alexandre.d.paquette@gmail.com>
2023-12-20 15:02:20 +0000

    Added constructor to Reading class

--- Models/Reading.cs
+++ Models/Reading.cs
@@ -1,6 +1,12 @@
 ﻿namespace MeterReader.Models;
 public class Reading
 {
+    public Reading(Meter energyMeter, DateTime time, decimal amount)
+    {
+        EnergyMeter = energyMeter;
+        Time = time;
+        Amount = amount;
+    }
     public Meter EnergyMeter { get; private set; }
     public DateTime Time { get; private set; }
     public decimal Amount { get; private set; }
