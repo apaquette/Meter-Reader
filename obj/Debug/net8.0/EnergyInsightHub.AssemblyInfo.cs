32f0d7498e6b62541fbfd61d73966269a29afddc
Alex <alexandre.d.paquette@gmail.com>
2023-12-22 13:17:44 +0000

    Scaffolding

--- obj/Debug/net8.0/EnergyInsightHub.AssemblyInfo.cs
+++ obj/Debug/net8.0/EnergyInsightHub.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EnergyInsightHub")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d45d4529b2ba197ef64ac141608acebdb24b4bdf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("EnergyInsightHub")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EnergyInsightHub")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
