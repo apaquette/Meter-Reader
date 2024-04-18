e8a117638ee65cb4a87bbf80a9067b2d3d5487b2
Alex <alexandre.d.paquette+github@gmail.com>
2024-04-18 17:56:38 +0100

    Fixed bug where python process would not end, and new process would start each time the app started up. pid is now stored and tracked, and process is killed if it's still running when the app starts so it can be started up again

--- EnergyInsightHub/Services/MeterReaderService.cs
+++ EnergyInsightHub/Services/MeterReaderService.cs
@@ -1,56 +1,74 @@
 ﻿namespace EnergyInsightHub.Services;
+
+using System;
 using System.Diagnostics;
+using System.Management;
 
 public class MeterReaderService {
     private Process _meterReader;
-    private string _processName = "MeterReader";
+    private int? _processId;
 
     public MeterReaderService() {
-        RestartService(_processName);
+        _processId = GetPid();
+        StopMeterReader();
         StartMeterReader();
     }
 
     public void StartMeterReader() 
     {
-        // only start script if it's not already running
-        if(!IsProcessRunning(_processName)) {
-            string directoryPath = "MeterReader"; // Path to meter reader script
-            string scriptName = "MeterReader.py";       // Meter Reader script
-            string command = $"py {scriptName}";        // command to execute
+        string directoryPath = "MeterReader"; // Path to meter reader script
+        string scriptName = "MeterReader.py";       // Meter Reader script
+        string command = $"py {scriptName}";        // command to execute
 
-            // Create a process start info object
-            ProcessStartInfo startInfo = new() {
-                FileName = "cmd.exe",
-                WorkingDirectory = directoryPath,
-                Arguments = $"/C {command}",
-                UseShellExecute = false,
-                RedirectStandardOutput = true,
-                CreateNoWindow = true
-            };
+        // Create a process start info object
+        ProcessStartInfo startInfo = new() {
+            FileName = "cmd.exe",
+            WorkingDirectory = directoryPath,
+            Arguments = $"/C {command}",
+            UseShellExecute = false,
+            RedirectStandardOutput = true,
+            CreateNoWindow = true
+        };
 
-            _meterReader = Process.Start(startInfo);
+        _meterReader = Process.Start(startInfo);
+        File.WriteAllTextAsync(Path.Combine("pid.txt"), _meterReader.Id.ToString());
+    }
+
+    public void StopMeterReader() {
+        if (_processId.HasValue) {
+            int pid = (int)_processId;
+            EndService(pid);
         }
     }
 
-    public void StopMeterReader() 
+    private void EndService(int pid) {
+        ManagementObjectSearcher processSearcher = new ($"Select * From Win32_Process Where ParentProcessID={pid}");
+        ManagementObjectCollection processCollection = processSearcher.Get();
+
+        try {
+            Process proc = Process.GetProcessById(pid);
+            if (!proc.HasExited) proc.Kill();
+        }
+        catch (ArgumentException) {
+            // Process already exited.
+        }
+
+        if (processCollection != null) {
+            foreach (ManagementObject mo in processCollection) {
+                EndService(Convert.ToInt32(mo["ProcessID"])); //kill child processes(also kills childrens of childrens etc.)
+            }
+        }
+
+    }
+
+    private int? GetPid() 
     {
-        if(_meterReader != null && !_meterReader.HasExited ) {
-            _meterReader.Kill();
-            _meterReader?.Dispose();
+        if (File.Exists("pid.txt")) {
+            string pidString = File.ReadAllText("pid.txt");
+            if (int.TryParse(pidString, out int pid)) {
+                return pid;
+            }
         }
-    }
-
-    // check if the process is running
-    private bool IsProcessRunning(string processName) {
-        Process[] processes = Process.GetProcessesByName(processName);
-        return processes.Length > 0;
-    }
-
-    private void RestartService(string processName) {
-        Process[] processes = Process.GetProcessesByName(processName);
-        foreach( Process process in processes ) {
-            process.Kill();
-            process?.Dispose();
-        }
+        return null;
     }
 }
